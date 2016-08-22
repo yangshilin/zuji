74d3c86ef56d8139d9a541bc035c22dfbe21565a
yangshilin <490453207@qq.com>
2016-08-22 18:21:19 +0800

    first day

---
+++ ZuJi/src/com/example/zuji/Fragment_shequ.java
@@ -0,0 +1,17 @@
+package com.example.zuji;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragment_shequ extends Fragment {
+@Override
+public View onCreateView(LayoutInflater inflater, ViewGroup container,
+		Bundle savedInstanceState) {
+	// TODO Auto-generated method stub
+	View v=inflater.inflate(R.layout.activity_shequ, null, false);
+	return v;
+}
+}
